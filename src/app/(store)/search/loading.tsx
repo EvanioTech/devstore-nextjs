08b283500fd3ea8c70ee09ed6bfe009124728859
Evanio Tech <evanioshark@gmail.com>
2025-04-25 08:04:46 -0300

    update in animate loading in page search products

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,8 +1,31 @@
-export default function Loading() {
-    return (
-        <div>
-            <h1>carregando...</h1>
+'use client'
 
+import { LoadingSkeleton } from "@/components/skeleton"
+import { useSearchParams } from "next/navigation"
+
+export default function Loading() {
+
+    const searchParams = useSearchParams()
+    const query = searchParams.get('q')
+
+   
+
+    return (
+        <div className="flex flex-col gap-4">
+            <p className="text-sm">
+                resultados para: <span className="font-semibold">{query}</span>
+            </p>
+
+            <div className="grid grid-cols-3 gap-6">
+               <LoadingSkeleton className="h-[380px]" />
+               <LoadingSkeleton className="h-[380px]" />
+               <LoadingSkeleton className="h-[380px]" />
+               <LoadingSkeleton className="h-[380px]" />
+               <LoadingSkeleton className="h-[380px]" />
+               <LoadingSkeleton className="h-[380px]" />
+
+
+                </div>
             </div>
 
             )
