f52ffb069accf2e0238faca687379fc78debba79
Evanio Tech <evanioshark@gmail.com>
2025-04-25 08:57:40 -0300

    create test e2e

---
+++ cypress/e2e/add-product-to-cart.cy.ts
@@ -0,0 +1,50 @@
+describe('add product to cart', () => {
+  it('should be able to navigate to the product page and add it to the cart', () => {
+    cy.visit('http://localhost:3000');
+
+    
+
+    cy.get('a[href*="/product/"]').first().click();
+
+    cy.get('button').contains('Adicionar ao carrinho').click();
+
+    cy.contains('Cart (1)').should('exist');
+
+   
+  })
+
+  it('should no count duplicated products on cart', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('a[href*="/product/"]').first().click();
+
+    cy.location('pathname').should('include', '/product/');
+
+    cy.get('button').contains('Adicionar ao carrinho').click();
+    cy.get('button').contains('Adicionar ao carrinho').click();
+   
+
+    cy.contains('Cart (2)').should('exist');
+
+    
+  })
+
+  it('should be able to search for a product and add it to the cart', () => {
+    cy.visit('http://localhost:3000');
+
+    cy.get('input[name=q]').type('moletom').parent('form').submit();
+
+    cy.get('a[href*="/product/"]').first().click();
+
+    cy.location('pathname').should('include', '/product/');
+
+    cy.get('button').contains('Adicionar ao carrinho').click();
+
+
+
+    cy.contains('Cart (1)').should('exist');
+  }
+  )
+
+  
+})
